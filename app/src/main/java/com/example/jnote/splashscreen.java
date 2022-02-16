74d1c11bbeb14237e27bb2a9b0221a816e1b2520
jacques28 <devjacques28i@gmail.com>
2022-02-16 22:55:07 +0000

    add all java classes

--- app/src/main/java/com/example/jnote/splashscreen.java
+++ app/src/main/java/com/example/jnote/splashscreen.java
@@ -1,2 +1,27 @@
-package com.example.jnote;public class splashscreen {
+package com.example.jnote;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        final Intent i = new Intent(splashscreen.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+
 }
